0b5db6c56f7625af077ac83fda0c23adbe13caf9
dofreemaconhoi1471 <157689272+dofreemaconhoi1471@users.noreply.github.com>
2024-01-28 00:04:37 +0700

    Update tail.sh

--- tail.sh
+++ tail.sh
@@ -1,34 +1,39 @@
-#configure.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+#configure.sh VNC_USER_PASSWORD VNC_PASSWORD
 
-#disable spotlight indexing
+#!/bin/bash
+
+# Check for successful commands
+set -e
+
+# Disable spotlight indexing
 sudo mdutil -i off -a
 
-#Create new account
+# Create new account
 sudo dscl . -create /Users/vncuser
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
 sudo dscl . -create /Users/vncuser RealName "VNC User"
 sudo dscl . -create /Users/vncuser UniqueID 1001
 sudo dscl . -create /Users/vncuser PrimaryGroupID 80
 sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-sudo dscl . -passwd /Users/vncuser $1
-sudo dscl . -passwd /Users/vncuser $1
+sudo dscl . -passwd /Users/vncuser "$1"
+sudo dscl . -passwd /Users/vncuser "$1"
 sudo createhomedir -c -u vncuser > /dev/null
 
-#Enable VNC
+# Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+# VNC password
+echo "$2" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+# Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install tail
+# Install Tailscale
 curl -O https://pkgs.tailscale.com/stable/Tailscale-1.58.2-macos.zip
 unzip Tailscale-1.58.2-macos.zip
-sudo mv Tailscale.app /Applications/
 
-#startservice
+# Move and start Tailscale
+sudo mv Tailscale.app /Applications/
 sudo /Applications/Tailscale.app/Contents/MacOS/Tailscale up
